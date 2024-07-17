6b51cd4adf69f5ef7c8eb1c13ad164702cb9a53e
Neiland85 <bookingnadarecords@gmail.com>
2024-07-18 01:13:29 +0200

    update lua erchive withthe init project estructure

---
+++ script.lua
@@ -0,0 +1,14 @@
+Configuracion-Base-Datos-AWS-azure-openai/
+├── src/
+│   ├── main.js
+│   ├── db/
+│   │   └── connection.js
+│   └── services/
+│       └── openaiService.js
+├── config/
+│   └── config.js
+├── .gitignore
+├── README.md
+├── package.json
+└── .env
+
